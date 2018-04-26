9a592ccc8f000b672e16598c47bee37eaf1a5e70
João Pedro <joaosouza@concrete.com.br>
2018-04-26 09:19:30 -0300

    Initial Commit

---
+++ TalkInstrumentedTests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TalkInstrumentedTests
+//
+//  Created by Joao Souza on 26/04/2018.
+//  Copyright © 2018 Concrete. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
